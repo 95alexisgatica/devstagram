6c44fcd5701526ff224e26d77f8ff90af69c6e63
alexis.gatica <alexis.gatica@outsourcearg.com>
2025-06-04 15:35:55 -0300

    Vista de Registro

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('principal');
 });
+
+Route::get('/crear-cuenta',[RegisterController::class, 'index']);
+Route::get('/autenticar',[RegisterController::class, 'autenticar']);
 
